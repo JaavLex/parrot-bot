c70d8113be8fe96da3c62e94e8f9457186f30ac0
Alexandre Javet <alexandre.javet@hotmail.com>
2020-10-08 04:50:32 -0700

    Update handler/message.js

--- handler/message.js
+++ handler/message.js
@@ -1,15 +1,19 @@
 async function onMessage(message) {
-  if (message.author.bot || !message.content.startsWith(prefix)) return;
+ if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g)
+  const cmd = args.shift().toLowerCase();
 
-  let command = client.commands.get(args);
+  let command = client.commands.get(cmd);
 
   if (!command) {
-    command = client.commands.get(client.aliases.get(args));
+    command = client.commands.get(client.aliases.get(cmd));
+  } else {
+    command.run(client, message, args);
   }
-
-  if (command) command.run(client, message, args);
 }
 
 module.exports = onMessage;
