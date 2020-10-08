354bf33c17d7afd2e25b7acca5d5c0d4eede3e2c
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-08 11:48:51 +0200

    refactor

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,24 +1,29 @@
-const pingCommand = {
-    name: 'ping',
-    category: 'info',
-    description: 'Returns bot\'s latency and API\' latency',
-    run: async (client, message, args) => {
-        const {
-            MessageEmbed
-        } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 
-        const msg = await message.channel.send(new MessageEmbed()
-            .setColor('#ff9900')
-            .setTitle('Latency')
-            .addField('✍️ Pinging ...', 'Results will be delivered shortly !'));
-        const newmsg = new MessageEmbed()
-            .setColor('#ff9900')
-            .setTitle('🌐 Latency 🌐')
-            .addField('🤖 Bot Latency :', `**${Math.floor(msg.createdAt - message.createdAt)}** ms`)
-            .addField('📄 API Latency :', `**${Math.round(client.ws.ping)}** ms`)
-            .setTimestamp();
-        msg.edit(newmsg);
-    }
+async function run(client, message, args) {
+  const msg = await message.channel.send(
+    new MessageEmbed()
+      .setColor('#ff9900')
+      .setTitle('Latency')
+      .addField('✍️ Pinging ...', 'Results will be delivered shortly !'),
+  );
+  const newmsg = new MessageEmbed()
+    .setColor('#ff9900')
+    .setTitle('🌐 Latency 🌐')
+    .addField(
+      '🤖 Bot Latency :',
+      `**${Math.floor(msg.createdAt - message.createdAt)}** ms`,
+    )
+    .addField('📄 API Latency :', `**${Math.round(client.ws.ping)}** ms`)
+    .setTimestamp();
+  msg.edit(newmsg);
+}
+
+const pingCommand = {
+  name: 'ping',
+  category: 'info',
+  description: "Returns bot's latency and API' latency",
+  run,
 };
 
 module.exports = pingCommand;
