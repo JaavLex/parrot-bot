afc835b5f4ff0bf5b73f42a3681770c773dc37ff
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-08 16:35:08 +0200

    add base

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,69 +1,83 @@
 const { MessageEmbed } = require('discord.js');
-const { stripIndents } = require('common-tags');
 const { prefix } = require('../../config.json');
 
-async function run(client, message, args) {
-  if (args[0]) {
-    return getCmd(client, message, args[0]);
-  } else {
-    return getAll(client, message);
+function getAllCommands(client, message) {
+  const embed = new MessageEmbed()
+    .setColor('#32CD32')
+    .setTitle('**Command list :**')
+    .setFooter(
+      `Type ${prefix}help [commandname] to have more info on a specific command.`,
+    )
+    .setThumbnail(
+      'https://i2.wp.com/thesecuritynoob.com/wp-content/uploads/2020/02/632px-Parrot_Logo.png?fit=632%2C599&ssl=1',
+    )
+    .setTimestamp();
+
+  function getCommandsByCategory(category) {
+    console.log('bonsoir', category);
+    return (
+      '`' +
+      client.commands.filter(cmd => cmd.category === category).join(' | ') +
+      '`'
+    );
   }
 
-  function getAll(client, message) {
-    const embed = new MessageEmbed()
-      .setColor('#32CD32')
-      .setTitle('**Command list :**')
-      .setFooter(
-        `Type ${prefix}help [commandname] to have more info on a specific command`,
-      )
-      .setThumbnail(
-        'https://i2.wp.com/thesecuritynoob.com/wp-content/uploads/2020/02/632px-Parrot_Logo.png?fit=632%2C599&ssl=1',
-      )
-      .setTimestamp();
-
-    const commands = category => {
-      return client.commands
+  function commandsListToString(category) {
+    return (
+      '```css\n' +
+      client.commands
         .filter(cmd => cmd.category === category)
-        .map(cmd => `- \`${cmd.name}\``)
-        .join('\n');
-    };
-
-    const info = client.categories
-      .map(
-        cat =>
-          stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(
-            cat,
-          )}`,
-      )
-      .reduce((string, category) => string + '\n' + category);
-
-    return message.channel.send(embed.setDescription(info));
+        .map(cmd => cmd.name)
+        .join(' | ') +
+      '\n```'
+    );
   }
 
-  function getCmd(client, message, input) {
-    const embed = new MessageEmbed();
-
-    const cmd =
-      client.commands.get(input.toLowerCase()) ||
-      client.commands.get(client.aliases.get(input.toLowerCase()));
-
-    let info = `No info for command : **${input.toLowerCase()}**`;
-
-    if (!cmd) {
-      return message.channel.send(embed.setColor('RED').setDescription(info));
-    }
-
-    if (cmd.name) info = `**Command name**: ${cmd.name}`;
-    if (cmd.aliases)
-      info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(', ')}`;
-    if (cmd.description) info += `\n**Description**: ${cmd.description}`;
-    if (cmd.usage) {
-      info += `\n**Usage**: ${cmd.usage}`;
-      embed.setFooter(`Syntax: <> = required, [] = optional`);
-    }
-
-    return message.channel.send(embed.setColor('GREEN').setDescription(info));
+  function cat(category) {
+    embed.addField(
+      '> ' + category.toUpperCase() || '-',
+      commandsListToString(category) || '-',
+    );
   }
+
+  const info = client.categories
+    .map(cat)
+    .reduce((string, category) => string + '\n' + category);
+
+  return message.channel.send(embed);
+}
+
+// function getSingleCommand(client, message, input) {
+//   const embed = new MessageEmbed();
+
+//   const cmd =
+//     client.commands.get(input.toLowerCase()) ||
+//     client.commands.get(client.aliases.get(input.toLowerCase()));
+
+//   let info = `No info for command : **${input.toLowerCase()}**`;
+
+//   if (!cmd) {
+//     return message.channel.send(embed.setColor('RED').setDescription(info));
+//   }
+
+//   if (cmd.name) info = `**Command name**: ${cmd.name}`;
+//   if (cmd.aliases)
+//     info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(', ')}`;
+//   if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+//   if (cmd.usage) {
+//     info += `\n**Usage**: ${cmd.usage}`;
+//     embed.setFooter(`Syntax: <> = required, [] = optional`);
+//   }
+
+//   return message.channel.send(embed.setColor('GREEN').setDescription(info));
+// }
+
+async function run(client, message, args) {
+  // if (args[0]) {
+  //   return getSingleCommand(client, message, args[0]);
+  // } else {
+  return getAllCommands(client, message);
+  // }
 }
 
 const helpCommand = {
