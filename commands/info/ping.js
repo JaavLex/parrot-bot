419fb1426104f38ee14890661a57857a8c91a6aa
TacticsCH <alexandre.javet@hotmail.com>
2020-10-08 11:06:05 +0200

    [FEAT.] Ping command is now fully functionnal
    - Changed from RichEmbed to MessageEmbed, (Changed since discord.js update)
    - Changed the code to calculate API's latency (Changed since discord.js update)
    - Added emojis to the final message in order to make it prettier
    - Changed the initial message ("pinging") into a MessageEmbed due to a bug. (When we edit the message with an embed, if the initial message was not an embed it is not deleted and stays at the top of the new message).
    - Added bold text to make the final message prettier
    - HOTFIXED index.js, calling a command was not working. args variable took only argument. I created a new variable with .shift() which permit to take only the command written by the user.

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,21 +1,21 @@
 module.exports = {
     name: 'ping',
     category: 'info',
-    description: 'Returns latency and API ping',
+    description: 'Returns bot\'s latency and API\' latency',
     run: async (client, message, args) => {
         const {
             MessageEmbed
         } = require('discord.js');
 
-        const msg = await message.channel.send(`✍️ Pinging ... ✍️`);
-        const newmsg = new MessageEmbed()
+        const msg = await message.channel.send(new MessageEmbed()
             .setColor('#ff9900')
             .setTitle('Latency')
-            .addField(
-                'Bot Latency :',
-                `${Math.floor(msg.createdAt - message.createdAt)} ms`,
-            )
-            .addField('API Latency :', `${Math.round(client.ping)} ms`)
+            .addField('✍️ Pinging ...', 'Results will be delivered shortly !'));
+        const newmsg = new MessageEmbed()
+            .setColor('#ff9900')
+            .setTitle('🌐 Latency 🌐')
+            .addField('🤖 Bot Latency :', `**${Math.floor(msg.createdAt - message.createdAt)}** ms`)
+            .addField('📄 API Latency :', `**${Math.round(client.ws.ping)}** ms`)
             .setTimestamp();
         msg.edit(newmsg);
     },
