6e56af8bb93bbe23c838b189566ed291a33d50ab
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-12 20:26:18 +0200

    add /ottersay

---
+++ commands/say/ottersay.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+const createEmbed = require('../../utils/disocrdUtils');
+const generateSayText = require('../../utils/sayUtils');
+
+async function run(client, message, args) {
+  const sentence = args.join(' ') || 'oooooooo oooo OO OO';
+
+  const embed = createEmbed(
+    '#ff9900',
+    `🦦 ${message.author.username} invoked **Baby Otter**`,
+    `Asked by ${message.author.username}`,
+  );
+
+  const otter = `
+       \\   .-"""-.
+        \\ /      o\\
+         |    o   0).-.
+         |       .-;(_/     .-.
+          \     /  /)).---._|  \`\\   ,
+           '.  '  /((       \`'-./ _/|
+             \  .'  )        .-.;\`  /
+              '.             |  \`\\-'
+                 '._        -'    /
+                   \`\`""--\`------\'    `;
+
+  embed.setDescription('```\n' + generateSayText(sentence) + otter + '\n```');
+
+  message.channel.send(embed);
+}
+
+const sayotterCommand = {
+  name: 'ottersay',
+  category: 'say',
+  description: 'You invoke a otter to say what you want !',
+  autoMessageDeletion: true,
+  run,
+};
+
+module.exports = sayotterCommand;
