329cc5a6bfa23365909a8ae323563d2d90f755a0
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-21 19:11:46 +0200

    add cat say

---
+++ commands/say/catsay.js
@@ -0,0 +1,46 @@
+const { createUserEmbed } = require('../../utils/discordUtils');
+const { createMdBlock } = require('../../utils/functions');
+const generateSayText = require('../../utils/sayUtils');
+
+async function run(client, message, args) {
+  const sentence = args.join(' ') || 'Sweet dreamy Miaou ';
+
+  const embed = createUserEmbed(
+    '#ff9900',
+    `🐈 ${message.author.username} invoked **Catinou**`,
+    {
+      command: catsayCommand.name,
+      author: message.author,
+    },
+  );
+
+  const chicken = `
+  \\   ,_     _
+   \\  |\\\\_,-~/
+    \\ / _  _ |    ,--.
+     (  @  @ )   / ,-'
+      \\  _T_/-._( (
+      /         \`. \\
+     |         _  \\ |
+      \\ \\ ,  /      |
+       || |-_\\__   /
+      ((_/\`(____,-'
+`;
+
+  embed.setDescription(
+    createMdBlock(generateSayText(sentence, chicken.length) + chicken),
+  );
+
+  await message.channel.send(embed);
+}
+
+const catsayCommand = {
+  name: 'catsay',
+  category: 'say',
+  aliases: ['casay'],
+  description: 'You invoke a cat to say what you want!',
+  autoMessageDeletion: true,
+  run,
+};
+
+module.exports = catsayCommand;
