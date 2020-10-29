57c5ccdfbc3eb3f29fd56f7f8de8cb7d8cfc1a5c
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-29 17:29:59 +0100

    edit texts

--- commands/miscellaneous/double.js
+++ commands/miscellaneous/double.js
@@ -1,27 +1,29 @@
+const { prefix } = require('../../utils/utils');
+
 async function run(client, message, args) {
   const sentence = args.join(' ') || 'Double parrot bot';
 
-  message.channel.send(getDoubleText(sentence))
+  message.channel.send(getDoubleText(sentence));
 }
 
 function getDoubleText(text) {
-  let doubleText = "";
+  let doubleText = '';
   for (let i = 0; i < text.length; i++) {
-    if (text[i] !== " ") {
-      doubleText += text[i] + text[i]
+    if (text[i] !== ' ') {
+      doubleText += text[i] + text[i];
     } else {
-      doubleText += " "
+      doubleText += ' ';
     }
   }
 
-  return doubleText
+  return doubleText;
 }
 
 const doubleCommand = {
   name: 'double',
   category: 'miscellaneous',
   aliases: ['dbl'],
-  description: "Send you're message with double char.\n\nExemple :\n> /double hello\n$ hheelllloo",
+  description: `Send your message with double char.\n\nExemple :\n> ${prefix}double hello\n$ hheelllloo`,
   usage: '<text>',
   autoMessageDeletion: false,
   run,
