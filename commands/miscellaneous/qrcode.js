e70cbe14df67f4c97547a3143112f87054a574b2
Melvynx <melvyn.malherbe@qoqa.com>
2020-10-30 15:07:19 +0100

    add qrcode command

---
+++ commands/miscellaneous/qrcode.js
@@ -0,0 +1,28 @@
+const { createUserEmbed } = require('../../utils/discordUtils');
+
+async function run(client, message, args) {
+  const link = args.join(' ') || 'Hi from parrot bot in qrcode :o !';
+
+  await message.channel.send(
+    createUserEmbed('#8e44ad', '👌 Your qrcode here 💪', {
+      author: message.author,
+      command: qrcodeCommand.command,
+    })
+      .setDescription('The qrcode may take time to load.')
+      .setImage(
+        `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${link}`,
+      ),
+  );
+}
+
+const qrcodeCommand = {
+  name: 'qrcode',
+  category: 'miscellaneous',
+  aliases: ['qr', 'qcode', 'rcode'],
+  description: 'Generate a qrcode !',
+  usage: '<qr code link>',
+  autoMessageDeletion: false,
+  run,
+};
+
+module.exports = qrcodeCommand;
