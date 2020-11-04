99369a1901b7b6d80d673531224d60e6ede0acba
Alex <alexandre.javet@hotmail.com>
2020-11-04 10:06:45 +0100

    [add] gif and quotation block

--- commands/miscellaneous/tronald.js
+++ commands/miscellaneous/tronald.js
@@ -1,7 +1,32 @@
+const giphy = require('giphy-api')();
 const fetch = require('node-fetch');
 const { createUserEmbed } = require('../../utils/discordUtils');
+const { createError } = require('../../utils/errorUtils');
+const { randomNumber, createMdBlock } = require('../../utils/utils');
 
 async function run(client, message, args) {
+  const imageUrl = await giphy.search('Donald Trump').then(response => {
+    if (!response || !response.data) {
+      throw createError(
+        'An error was encountered.',
+        '',
+        'Retry command !',
+        true,
+      );
+    }
+
+    const imageData = response.data[randomNumber(0, 25)];
+    if (!imageData) {
+      throw createError(
+        'No results to your query were found!',
+        'Your query could not be found by the giphy API.',
+        'Try to specify your query in a short specific keyword',
+        true,
+      );
+    }
+    return imageData.images.original.url;
+  });
+
   fetch('https://www.tronalddump.io/random/quote')
     .then(res => res.json())
     .then(async json => {
@@ -9,7 +34,9 @@ async function run(client, message, args) {
         createUserEmbed('#8e44ad', "👱 Here's your Donald Trump quote 👱", {
           author: message.author,
           command: tronaldCommand.command,
-        }).setDescription(json.value),
+        })
+          .setDescription(`> ${json.value}`)
+          .setImage(imageUrl),
       );
     });
 }
