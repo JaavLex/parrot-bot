65a87d0cfe28f28d1b03c80e06f5aeff2eea70d1
Melvynx <melvyn.malherbe@qoqa.com>
2020-11-07 14:25:20 +0100

    [ref.] fix admin test

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -4,15 +4,14 @@ async function run(client, message, args) {
   await message.channel.send('salut');
 }
 
-const chooseCommand = {
+const testAdmin = {
   name: 'test-admin',
   aliases: ['ta'],
   category: 'ask',
-  description: 'Chooses someone based on the question asked !',
-  usage: '[question]',
+  description: 'Test admin',
   autoMessageDeletion: false,
   permissions: [Permissions.MANAGE_CHANNELS, Permissions.KICK_MEMBERS],
   run,
 };
 
-module.exports = chooseCommand;
+module.exports = testAdmin;
