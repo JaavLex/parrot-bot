9642a674db2e80143f899c29951d64fddb979be7
Villou <dkulic01@gmail.com>
2020-11-09 14:44:36 +0100

    command avatar

---
+++ commands/info/avatar.js
@@ -0,0 +1,33 @@
+const {
+  createUserEmbed,
+  replaceDiscordTag,
+} = require('../../utils/discordUtils');
+
+async function run(client, message, args) {
+  const userMentioned = message.mentions.users.first() || message.author;
+
+  const embed = createUserEmbed('#f39c12', '', {
+    author: message.author,
+  })
+    .setImage(
+      userMentioned.displayAvatarURL({
+        dynamic: true,
+        format: 'png',
+        size: 512,
+      }),
+    )
+    .setAuthor(userMentioned.username);
+
+  await message.channel.send(embed);
+}
+
+const avatarCommand = {
+  name: 'avatar',
+  category: 'info',
+  aliases: ['pic', 'pp'],
+  description: 'Display the avatar of mentioned user',
+  autoMessageDeletion: false,
+  run,
+};
+
+module.exports = avatarCommand;
