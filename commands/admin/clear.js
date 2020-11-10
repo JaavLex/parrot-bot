21b08b3d0ad42fab8e8c6262c58b3863332e81e6
Villou <dkulic01@gmail.com>
2020-11-10 19:23:21 +0100

    clear 10 msg

---
+++ commands/admin/clear.js
@@ -0,0 +1,19 @@
+const { Permissions } = require('../../utils/adminUtils');
+
+async function run(client, message, args) {
+  await message.channel.bulkDelete(11).then(() => {
+    message.channel.send('Deleted 10 messages.');
+  });
+}
+
+const clearCommand = {
+  name: 'clear',
+  aliases: ['purge'],
+  category: 'admin',
+  description: 'Delete the last 10 messages',
+  autoMessageDeletion: false,
+  permissions: [Permissions.MANAGE_CHANNELS],
+  run,
+};
+
+module.exports = clearCommand;
