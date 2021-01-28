74582d4fd3cb225c49b10cd4cd591ccc09ef84bd
Melvynx <melvyn.malherbe@qoqa.com>
2021-01-28 10:50:08 +0100

    add review command

---
+++ commands/dev/review-done.js
@@ -0,0 +1,47 @@
+const approvedList = ['ap', 'approuved', 'a', 'done'];
+const requestChangeList = [
+  'rc',
+  'requestchange',
+  'needchange',
+  'change',
+  'edit',
+];
+
+function getPullRequestTextReview(result, message) {
+  if (approvedList.includes(result)) {
+    return `✅ ${message.author.username} approved the Pull Request #`;
+  }
+  if (requestChangeList.includes(result)) {
+    return `❌ ${message.author.username} request change to the Pull Request #`;
+  }
+  return `🎬 ${message.author.username} comment the Pull Request #`;
+}
+
+async function run(client, message, args) {
+  const reviewResult = args[0];
+  const reviewId = args[1];
+
+  let textMessage = getPullRequestTextReview(reviewResult, message);
+  textMessage += reviewId || reviewResult;
+
+  await message.channel.send(textMessage);
+}
+
+const gayrateCommand = {
+  name: 'review',
+  aliases: ['re', 'review-done', 'rev', 'revi', 'revie'],
+  category: 'dev',
+  description: `For developer inform review of pull request.
+
+  if approved review result can be : \`['ap', 'approuved', 'a', 'done']\`
+  if request change result can be : \`['rc', 'requestchange', 'needchange', 'change', 'edit']\`
+  if just comment result can be what you want.
+
+  if you don't put the \`reviewId\` the message can be bug.`,
+  usage: '[reviewResult or reviewId] [reviewId]',
+  autoMessageDeletion: true,
+  customDescription: true,
+  run,
+};
+
+module.exports = gayrateCommand;
